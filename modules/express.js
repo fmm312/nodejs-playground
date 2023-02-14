349f7402c81a6d1eb2389972ffab62d48594afac
Felipe Menezes <fmm312@gmail.com>
2023-02-14 19:02:50 -0300

    Create a crud

--- modules/express.js
+++ modules/express.js
@@ -1,6 +1,9 @@
-const express = require('express');
+const express = require("express");
+const UserModel = require("../src/models/user.model");
 
 const app = express();
+app.use(express.json());
+
 const port = 8080;
 
 app.get("/home", (req, res) => {
@@ -8,21 +11,54 @@ app.get("/home", (req, res) => {
     res.status(200).send("<h1>Hello World</h1>");
 });
 
-app.get("/users", (req, res) => {
-    const users = [
-        {
-            name: "John Doe",
-            email: "john@doe.com"
-        },
-        {
-            name: "Jane Doe",
-            email: "jane@doe.com"
-        }
-    ]
+app.get("/users", async (req, res) => {
+    try {
+        const users = await UserModel.find({});
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
 
-    res.status(200).json(users);
+app.get("/users/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findById(id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.post("/users", async (req, res) => {
+    try {
+        const user = await UserModel.create(req.body);
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.patch("/users/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+        res.status(200).json(user);
+    } catch {
+        res.status(500).send(error.message);
+    }
+});
+
+app.delete("/users/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndRemove(id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 });
 
 app.listen(port, () => {
     console.log(`Rodando com Express na porta ${port}!`)
-})
+});
