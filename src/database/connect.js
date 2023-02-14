44963ac9ef6c539a10b9b498b936204fb71dbb63
Felipe Menezes <fmm312@gmail.com>
2023-02-14 18:06:43 -0300

    Connecting with mongo database

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
-    await mongoose.connect("mongodb+srv://admin:<password>@nodejsplayground.pbajido.mongodb.net/?retryWrites=true&w=majority", 
+    await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@nodejsplayground.pbajido.mongodb.net/?retryWrites=true&w=majority`, 
     (error) => {
         if (error) {
             return console.log('Ocorreu um erro ao se conectar com o banco de dados: ', error);
